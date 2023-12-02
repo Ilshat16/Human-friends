5800e7350c2143f850abb8e28608c0b7f690f644
ilshat16 <illliat@mail.ru>
2023-12-02 19:15:20 +0300

    Initial commit

---
+++ src/main/java/controlwork/human_friend/HumanFriendsApplication.java
@@ -0,0 +1,13 @@
+package controlwork.human_friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/controlwork/human_friend/HumanFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package controlwork.human_friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
